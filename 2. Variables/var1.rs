9333d1542c37acdfc7cea073cee53d467646e935
Aditya Mishra <mishraaaditya29@gmail.com>
2023-12-28 20:45:42 +0530

    Var1 Added

---
+++ 2. Variables/var1.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let x: i32 = 5; // Uninitialized but used, ERROR !
+    let y: i32; // Uninitialized but also unused, only a Warning !
+
+    assert_eq!(x, 5); // If equal to 5, then output is Success!
+    println!("Success!");
+}
