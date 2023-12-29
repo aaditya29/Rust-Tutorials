537925c1d3a45ecc7cba8731279c80d39963c1e4
Aditya Mishra <mishraaaditya29@gmail.com>
2023-12-29 10:51:27 +0530

    var5.rs added

---
+++ 2. Variables/var5.rs
@@ -0,0 +1,14 @@
+//Shadowing
+
+fn main() {
+    let x: i32 = 5;
+    {
+        let x = 12;
+        assert_eq!(x, 12);
+    }
+
+    assert_eq!(x, 5);
+
+    let x = 42;
+    println!("{}", x); // Prints "42".
+}
