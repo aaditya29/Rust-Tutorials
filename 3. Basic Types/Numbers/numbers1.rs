f48bdc982decb2639125c5036b8d745b1092bee2
Aditya Mishra <mishraaaditya29@gmail.com>
2024-01-03 12:10:18 +0530

    Num1.rs Completed

--- 3. Basic Types/Numbers/numbers1.rs
+++ 3. Basic Types/Numbers/numbers1.rs
@@ -1,1 +1,11 @@
+// Remove something to make it work
+fn main() {
+    let x: i32 = 5;
+    let mut y = 5; // takes by default as i32
 
+    y = x;
+
+    let z: i32 = 10; // Type of z is i32
+
+    println!("Success!");
+}
