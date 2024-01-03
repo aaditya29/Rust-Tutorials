cb893f43d396cedd4b779a91ec8d0d6c70c76c2b
Aditya Mishra <mishraaaditya29@gmail.com>
2024-01-03 13:50:14 +0530

    numbers9.rs completed

---
+++ 3. Basic Types/Numbers/numbers9.rs
@@ -0,0 +1,32 @@
+/*
+Two goals:
+1. Modify assert! to make it work
+2. Make println! output: 97 - 122
+
+fn main() {
+    let mut sum = 0;
+    for i in -3..2 {
+        sum += i
+    }
+
+    assert!(sum == -3);
+
+    for c in 'a'..='z' {
+        println!("{}",c);
+    }
+}
+
+*/
+
+fn main() {
+    let mut sum = 0;
+    for i in -3..2 {
+        sum += i
+    }
+
+    assert!(sum == -5);
+
+    for c in 'a'..='z' {
+        println!("{}", c as u8); //Printing ASCII characters from 97 to 122
+    }
+}
