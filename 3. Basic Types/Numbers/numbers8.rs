22a3b51eb174d098a5480baf7658deb8bb49d196
Aditya Mishra <mishraaaditya29@gmail.com>
2024-01-03 13:46:15 +0530

    numbers8.rs completed

---
+++ 3. Basic Types/Numbers/numbers8.rs
@@ -0,0 +1,17 @@
+/*
+Make it work in two distinct ways
+
+
+fn main() {
+    assert!(0.1+0.2==0.3);
+
+    println!("Success!");
+}
+
+*/
+
+fn main() {
+    assert!(0.1 as f32 + 0.2 as f32 == 0.3 as f32);
+
+    println!("Success!");
+}
