dc47d6028a5cb826f38665c1dc24b836bdc66198
Aditya Mishra <mishraaaditya29@gmail.com>
2024-01-03 13:23:42 +0530

    numbers5.rs completed

---
+++ 3. Basic Types/Numbers/numbers5.rs
@@ -0,0 +1,17 @@
+/*
+
+Fix errors and panics to make it work
+fn main() {
+   let v1 = 251_u8 + 8;
+   let v2 = i8::checked_add(251, 8).unwrap();
+   println!("{},{}",v1,v2);
+}
+
+*/
+
+fn main() {
+    let v1: u16 = 251_u16 + 8; // Max limit of u8 is 255 so 259 will overflow.
+    let v2: i16 = i16::checked_add(251, 8).unwrap; // Max limit of i16 is 255 so 259 will overflow.
+
+    println!("{}, {}", v1, v2);
+}
