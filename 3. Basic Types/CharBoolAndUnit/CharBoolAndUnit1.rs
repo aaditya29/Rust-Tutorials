8484827c8d94a5d43ec297d9b3317a0e4c2c66cd
Aditya Mishra <mishraaaditya29@gmail.com>
2024-01-04 09:45:26 +0530

    CharBoolAndUnit1.rs completed

--- 3. Basic Types/CharBoolAndUnit/CharBoolAndUnit1.rs
+++ 3. Basic Types/CharBoolAndUnit/CharBoolAndUnit1.rs
@@ -14,10 +14,10 @@ fn main() {
 */
 use std::mem::size_of_val;
 fn main() {
-    let c1 = 'a';
+    let c1: char = 'a';
     assert_eq!(size_of_val(&c1), 4); // In Rust char is of 4 bytes
 
-    let c2 = '中';
+    let c2: char = '中';
     assert_eq!(size_of_val(&c2), 4);
 
     println!("Success!");
