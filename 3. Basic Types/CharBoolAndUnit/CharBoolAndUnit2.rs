9ff533faa246ff0ce1b79d782380b7c2f1ab17a3
Aditya Mishra <mishraaaditya29@gmail.com>
2024-01-04 09:47:24 +0530

    CharBoolAndUnit2.rs Completed

---
+++ 3. Basic Types/CharBoolAndUnit/CharBoolAndUnit2.rs
@@ -0,0 +1,20 @@
+/*
+Make It Work
+
+fn main() {
+    let c1 = "中";
+    print_char(c1);
+}
+
+fn print_char(c : char) {
+    println!("{}", c);
+}
+*/
+fn main() {
+    let c1: char = '中';
+    print_char(c1);
+}
+
+fn print_char(c: char) {
+    println!("{}", c);
+}
