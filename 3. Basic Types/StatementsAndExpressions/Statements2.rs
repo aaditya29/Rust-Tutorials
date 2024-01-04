145b246b876160124754916c64985f0e409cfdb1
Aditya Mishra <mishraaaditya29@gmail.com>
2024-01-04 17:13:37 +0530

    Statements Completed

---
+++ 3. Basic Types/StatementsAndExpressions/Statements2.rs
@@ -0,0 +1,22 @@
+/*
+Make it work
+
+fn main() {
+   let v = (let x = 3);
+
+   assert!(v == 3);
+
+   println!("Success!");
+}
+
+*/
+fn main() {
+    let v: i32 = {
+        let x: i32 = 3;
+        x
+    };
+
+    assert!(v == 3);
+
+    println!("Success!");
+}
---
+++ 3. Basic Types/StatementsAndExpressions/Statements3.rs
@@ -0,0 +1,24 @@
+/*
+Meke it work
+
+
+fn main() {
+    let s = sum(1 , 2);
+    assert_eq!(s, 3);
+
+    println!("Success!");
+}
+
+fn sum(x: i32, y: i32) -> i32 {
+    x + y;
+}
+*/
+
+fn main() {
+    let s = sum(1, 2);
+    assert_eq!(s, 3);
+}
+
+fn sum(x: i32, y: i32) -> i32 {
+    x + y
+}
