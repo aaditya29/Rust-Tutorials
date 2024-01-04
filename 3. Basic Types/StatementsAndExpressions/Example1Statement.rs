493e7229ce14e7b794f4d0a0e5c85129684663ba
Aditya Mishra <mishraaaditya29@gmail.com>
2024-01-04 17:00:35 +0530

    Example 1  Of Statements Added

---
+++ 3. Basic Types/StatementsAndExpressions/Example1Statement.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let x: u32 = 5u32;
+
+    let y: u32 = {
+        let x_squared = x * x;
+        let x_cube = x_squared * x;
+
+        // This expression will be assigned to `y`
+        x_cube + x_squared + x //no semicolon because it shows the line is expression and will be assigned to y.
+    };
+
+    let z = {
+        // The semicolon suppresses this expression and `()` is assigned to `z`
+        2 * x;
+    };
+
+    println!("x is {:?}", x);
+    println!("y is {:?}", y);
+    println!("z is {:?}", z);
+}
