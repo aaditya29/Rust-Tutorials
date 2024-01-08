8eaca641e19c4903df880713ef47315b5ca73f3f
Aditya Mishra <mishraaaditya29@gmail.com>
2024-01-08 19:41:06 +0530

    ownership9.rs Added

---
+++ 4. Ownership And Borrowing/Ownership/ownership9.rs
@@ -0,0 +1,19 @@
+/*
+
+fn main() {
+   let t = (String::from("hello"), String::from("world"));
+
+     Fill the blanks
+    let (__, __) = __;
+
+    println!("{:?}, {:?}, {:?}", s1, s2, t); // -> "hello", "world", ("hello", "world")
+}
+*/
+
+fn main() {
+    let t: (String, String) = (String::from("hello"), String::from("world"));
+
+    let (s1, s2) = t.clone();
+
+    println!("{:?}, {:?}, {:?}", s1, s2, t); // -> "hello", "world", ("hello", "world")
+}
