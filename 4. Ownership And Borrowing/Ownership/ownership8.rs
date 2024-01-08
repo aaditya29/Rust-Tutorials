7fa54e6d0491c9d33817702ebbf8ffdee00b856c
Aditya Mishra <mishraaaditya29@gmail.com>
2024-01-08 19:39:35 +0530

    ownership8.rs added

--- 4. Ownership And Borrowing/Ownership/ownership8.rs
+++ 4. Ownership And Borrowing/Ownership/ownership8.rs
@@ -1,3 +1,20 @@
 /*
 
+fn main() {
+   let t = (String::from("hello"), String::from("world"));
+
+   let _s = t.0;
+
+   // Modify this line only, don't use `_s`
+   println!("{:?}", t);
+}
 */
+
+fn main() {
+    let t: (String, String) = (String::from("hello"), String::from("world"));
+
+    let _s: String = t.0;
+
+    // Modify this line only, don't use `_s`
+    println!("{:?}", t.1); //Calling second element of tuple
+}
