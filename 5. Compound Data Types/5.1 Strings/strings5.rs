43854854f5637d93a6094042e4b1a8034720c531
Aditya Mishra <mishraaaditya29@gmail.com>
2024-01-11 18:09:06 +0530

    strings questions added

---
+++ 5. Compound Data Types/5.1 Strings/strings5.rs
@@ -0,0 +1,13 @@
+//replace can be used to replace substring
+
+/*
+fn main() {
+    let s = String::from("I like dogs");
+    / Allocate new memory and store the modified string there
+    let s1 = s.__("dogs", "cats");
+
+    assert_eq!(s1, "I like cats");
+
+    println!("Success!");
+}
+ */
