e52c18b9f5cb955d4c058bf4a63fec3e3e895d2a
Aditya Mishra <mishraaaditya29@gmail.com>
2024-01-11 18:12:22 +0530

    strings questions added

---
+++ 5. Compound Data Types/5.1 Strings/strings7.rs
@@ -0,0 +1,12 @@
+/*
+
+/ Fix error with at least two solutions
+fn main() {
+    let s = "hello, world";
+    greetings(s)
+}
+
+fn greetings(s: String) {
+    println!("{}", s)
+}
+*/
---
+++ 5. Compound Data Types/5.1 Strings/strings8.rs
@@ -0,0 +1,10 @@
+/*
+
+/ Use two approaches to fix the error and without adding a new line
+fn main() {
+    let s = "hello, world".to_string();
+    let s1: &str = s;
+
+    println!("Success!");
+}
+*/
