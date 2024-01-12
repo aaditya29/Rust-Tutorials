d01ae73516b981466f369c5cf605188a0b73b52c
Aditya Mishra <mishraaaditya29@gmail.com>
2024-01-12 16:54:40 +0530

    strings2.rs solution added

--- 5. Compound Data Types/5.1 Strings/strings2.rs
+++ 5. Compound Data Types/5.1 Strings/strings2.rs
@@ -1,3 +1,5 @@
+//We can only use str by boxing it, & can be used to convert Box<str> to &str
+
 /*
 
 / Fix the error with at least two solutions
@@ -10,3 +12,12 @@ fn greetings(s: &str) {
     println!("{}",s)
 }
 */
+
+fn main() {
+    let s: Box<str> = "hello, world".into();
+    greetings(&s);
+}
+
+fn greetings(s: &str) {
+    println!("{}", s)
+}
