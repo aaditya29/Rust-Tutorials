b0c9ded68100484ae1c5796691d1091c151a9f3d
Aditya Mishra <mishraaaditya29@gmail.com>
2024-01-13 12:25:58 +0530

    arrays question folders added

---
+++ 5. Compound Data Types/5.2 Arrays/arrays1.rs
@@ -0,0 +1,12 @@
+/*
+
+fn main() {
+    / Fill the blank with proper array type
+    let arr: __ = [1, 2, 3, 4, 5];
+
+    / Modify the code below to make it work
+    assert!(arr.len() == 4);
+
+    println!("Success!");
+}
+*/
