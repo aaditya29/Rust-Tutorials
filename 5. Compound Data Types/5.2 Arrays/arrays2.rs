b5e034fe8d415e07e328f1b1c8025d39faf1f7e0
Aditya Mishra <mishraaaditya29@gmail.com>
2024-01-16 17:52:04 +0530

    solution 1 and solution 2 added

--- 5. Compound Data Types/5.2 Arrays/arrays2.rs
+++ 5. Compound Data Types/5.2 Arrays/arrays2.rs
@@ -13,3 +13,11 @@ fn main() {
     println!("Success!");
 }
 */
+fn main() {
+    let arr0 = [1, 2, 3];
+    let arr: [char; 3] = ['a', 'b', 'c'];
+
+    assert!(std::mem::size_of_val(&arr) == 12);
+
+    println!("Success!");
+}
