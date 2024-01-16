899ce047f97b4fd700d3e8492dc13143a3d392bc
Aditya Mishra <mishraaaditya29@gmail.com>
2024-01-16 18:02:03 +0530

    array6 solution added

--- 5. Compound Data Types/5.2 Arrays/arrays6.rs
+++ 5. Compound Data Types/5.2 Arrays/arrays6.rs
@@ -14,3 +14,13 @@ fn main() {
 }
 
 */
+
+fn main() {
+    let names: [String; 2] = [String::from("Sunfei"), "Sunface".to_string()];
+
+    let name0 = names.get(0).unwrap();
+
+    let _name1 = &names[0];
+
+    println!("Success!");
+}
