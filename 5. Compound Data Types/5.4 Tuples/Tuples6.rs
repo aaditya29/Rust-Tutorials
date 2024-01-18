9a165a772ca621ad1f0b4a693e4e239d65ff9137
Aditya Mishra <mishraaaditya29@gmail.com>
2024-01-18 16:23:25 +0530

    Tuples Questions Added

---
+++ 5. Compound Data Types/5.4 Tuples/Tuples6.rs
@@ -0,0 +1,17 @@
+//Tuples can be used as function arguments and return values
+/*
+
+fn main() {
+    / Fill the blank, need a few computations here.
+    let (x, y) = sum_multiply(__);
+
+    assert_eq!(x, 5);
+    assert_eq!(y, 6);
+
+    println!("Success!");
+}
+
+fn sum_multiply(nums: (i32, i32)) -> (i32, i32) {
+    (nums.0 + nums.1, nums.0 * nums.1)
+}
+*/
