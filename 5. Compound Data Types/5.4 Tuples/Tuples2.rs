e41d8d096c21e792c021fe35f6b7f5ed0947a25c
Aditya Mishra <mishraaaditya29@gmail.com>
2024-01-18 16:22:44 +0530

    Tuples Questions Added

---
+++ 5. Compound Data Types/5.4 Tuples/Tuples2.rs
@@ -0,0 +1,12 @@
+//Members can be extracted from the tuple using indexing.
+
+/*
+
+/ Make it work
+fn main() {
+    let t = ("i", "am", "sunface");
+    assert_eq!(t.1, "sunface");
+
+    println!("Success!");
+}
+*/
