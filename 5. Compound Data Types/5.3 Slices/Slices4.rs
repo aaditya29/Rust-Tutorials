09983aef1f10773b62d09cde437003c9e4ad919d
Aditya Mishra <mishraaaditya29@gmail.com>
2024-01-18 16:03:13 +0530

    slices4.rs solution added

--- 5. Compound Data Types/5.3 Slices/Slices4.rs
+++ 5. Compound Data Types/5.3 Slices/Slices4.rs
@@ -12,3 +12,11 @@ fn main() {
     println!("Success!");
 }
 */
+fn main() {
+    let s = String::from("hello");
+
+    let slice1 = &s[0..2];
+    let slice2 = &s[..2];
+
+    assert_eq!(slice1, slice2);
+}
