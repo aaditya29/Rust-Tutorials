2129807fa3083c555da6fd6f86b70e18570aa6fa
Aditya Mishra <mishraaaditya29@gmail.com>
2024-01-19 12:10:36 +0530

    tuples4.rs solved

--- 5. Compound Data Types/5.4 Tuples/Tuples4.rs
+++ 5. Compound Data Types/5.4 Tuples/Tuples4.rs
@@ -14,3 +14,14 @@ fn main() {
     println!("Success!");
 }
 */
+fn main() {
+    let tup = (1, 6.4, "hello");
+
+    let (x, z, y) = tup;
+
+    assert_eq!(x, 1);
+    assert_eq!(y, "hello");
+    assert_eq!(z, 6.4);
+
+    println!("Success!");
+}
