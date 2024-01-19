ae56fb066c261e49162b0769af43a93e468e28cc
Aditya Mishra <mishraaaditya29@gmail.com>
2024-01-19 12:13:11 +0530

    Tuples exercises completed

--- 5. Compound Data Types/5.4 Tuples/Tuples6.rs
+++ 5. Compound Data Types/5.4 Tuples/Tuples6.rs
@@ -15,3 +15,16 @@ fn sum_multiply(nums: (i32, i32)) -> (i32, i32) {
     (nums.0 + nums.1, nums.0 * nums.1)
 }
 */
+
+fn main() {
+    let (x, y) = sum_multiply((2, 3));
+
+    assert_eq!(x, 5);
+    assert_eq!(y, 6);
+
+    println!("Success!");
+}
+
+fn sum_multiply(nums: (i32, i32)) -> (i32, i32) {
+    (nums.0 + nums.1, nums.0 * nums.1)
+}
