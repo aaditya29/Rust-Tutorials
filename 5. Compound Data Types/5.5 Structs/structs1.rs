3d1863772f9b59467c63d0ed239096e26bd57993
Aditya Mishra <mishraaaditya29@gmail.com>
2024-01-26 19:50:48 +0530

    Structs Exercises Added

---
+++ 5. Compound Data Types/5.5 Structs/structs1.rs
@@ -0,0 +1,20 @@
+// We must specify concrete values for each of the fields in struct.
+
+/*
+
+/ Fix the error
+struct Person {
+    name: String,
+    age: u8,
+    hobby: String
+}
+fn main() {
+    let age = 30;
+    let p = Person {
+        name: String::from("sunface"),
+        age,
+    };
+
+    println!("Success!");
+}
+*/
