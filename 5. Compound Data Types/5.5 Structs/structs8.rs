bbbb4502e7c4b736dd6205f3db210ee9601cfb4f
Aditya Mishra <mishraaaditya29@gmail.com>
2024-05-15 10:36:02 +0530

    Structs8.rs Completed

--- 5. Compound Data Types/5.5 Structs/structs8.rs
+++ 5. Compound Data Types/5.5 Structs/structs8.rs
@@ -18,3 +18,19 @@ fn main() {
     println!("{}, {}, {:?}",f.name, f.data, f);
 }
 */
+#[derive(Debug)]
+struct File {
+    name: String,
+    data: String,
+}
+fn main() {
+    let f = File {
+        name: String::from("readme.md"),
+        data: "Rust By Practice".to_string(),
+    };
+
+    let _name = f.name;
+
+    // ONLY modify this line
+    println!("{}", f.data);
+}
