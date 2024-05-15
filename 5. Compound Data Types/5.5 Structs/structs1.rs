ba2213b2e7fc2290b40ccacd6465a2349d770dc1
Aditya Mishra <mishraaaditya29@gmail.com>
2024-05-15 10:16:05 +0530

    struct1.rs completed

--- 5. Compound Data Types/5.5 Structs/structs1.rs
+++ 5. Compound Data Types/5.5 Structs/structs1.rs
@@ -18,3 +18,19 @@ fn main() {
     println!("Success!");
 }
 */
+
+struct Person {
+    name: String,
+    age: u8,
+    hobby: String,
+}
+fn main() {
+    let age: u8 = 30; // defining datatype
+    let p: Person = Person {
+        name: String::from("sunface"),
+        age,
+        hobby: String::from("Movies"),
+    };
+
+    println!("Success!");
+}
